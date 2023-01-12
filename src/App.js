f8befa4ef6bbe66bf80207841f1d418ec2756c93
Shubham Maurya <skmaurya9721@gmail.com>
2023-01-12 17:11:47 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,169 +1,197 @@
-import {
-  Input,
-  SkeletonText,
-} from '@chakra-ui/react'
-import logo from './logo.png'
-import './App.css'
+import { Input, SkeletonText } from "@chakra-ui/react";
+import logo from "./logo.png";
+import "./App.css";
 import {
   useJsApiLoader,
   GoogleMap,
-  Marker,
-  Autocomplete,
   DirectionsRenderer,
-} from '@react-google-maps/api'
+} from "@react-google-maps/api";
 
 import { MdControlPoint } from "react-icons/md";
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from "react";
 
+import Map from "./map";
 
-
-const REACT_APP_GOOGLE_MAPS_API_KEY = "AIzaSyAolXVBph__8LXk-JukgnxDUI4LPDQAsxQ"
-const center = { lat: 48.8584, lng: 2.2945 }
+const REACT_APP_GOOGLE_MAPS_API_KEY = "AIzaSyAolXVBph__8LXk-JukgnxDUI4LPDQAsxQ";
+const center = { lat: 48.8584, lng: 2.2945 };
 
 function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ['places'],
-  })
+    libraries: ["places"],
+  });
 
-  const [map, setMap] = useState(/** @type google.maps.Map */(null))
-  const [directionsResponse, setDirectionsResponse] = useState(null)
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
-  const [orgine, setOrgine] = useState('')
-  const [end, setEnd] = useState('')
-  const [istrue, setTrue] = useState(false)
-  const [Allitem, setAllItem] = useState([])
-  const [item, setItem] = useState(<Autocomplete className="InputStyle">
-    <Input type='text' placeholder='Stop' style={{ backgroundColor: "white" }} />
-  </Autocomplete>);
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
 
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const [orgine, setOrgine] = useState("");
+  const [end, setEnd] = useState("");
+  const [istrue, setTrue] = useState(false);
+  const [Allitem, setAllItem] = useState([]);
+  const [item, setItem] = useState({ value: "" });
 
+  const [Latlag, setLatlag] = useState([]);
 
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef()
+  const originRef = useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef()
+  const destiantionRef = useRef();
 
   if (!isLoaded) {
-    return <SkeletonText />
+    return <SkeletonText />;
   }
 
-  async function calculateRoute() {
-    if (originRef.current.value === '' || destiantionRef.current.value === '') {
-      return
+  const CalculateLatLag = async () => {
+    let arr = [];
+    let demoarry = [];
+    for (let i = 0; i < Allitem.length; i++) {
+      demoarry.push(Allitem[i]);
     }
-    // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
-    const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destiantionRef.current.value,
-      // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration.text)
-    setEnd(destiantionRef.current.value)
-    setOrgine(originRef.current.value)
+
+    demoarry.push({ value: destiantionRef.current.value });
+    setOrgine(originRef.current.value);
+    setEnd(destiantionRef.current.value);
+    demoarry.unshift({ value: originRef.current.value });
+    console.log(demoarry, "hi");
+
+    for (let i = 0; i < demoarry.length; i++) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${demoarry[i].value}&key=AIzaSyAolXVBph__8LXk-JukgnxDUI4LPDQAsxQ`
+      );
+      const data = await response.json();
+      let lat = data.results[0].geometry.location.lat;
+      let lag = data.results[0].geometry.location.lng;
+      arr.push({ lat: lat, lag: lag });
+    }
+    setLatlag(arr);
+    console.log({ Latlag });
+  };
+
+  async function calculateRoute() {
+    CalculateLatLag();
     setTrue(true);
   }
   function AddStop() {
-    console.log("hi addStope")
-
-    setAllItem(...Allitem, item);
-
+    setAllItem([...Allitem, item]);
   }
 
+  const handleInput = (e, index) => {
+    let _allItem = [...Allitem];
+    _allItem[index] = { value: e.target.value };
+    setAllItem(_allItem);
+  };
+  console.log({ Allitem });
+
   return (
-
     <div className="App">
-      <div className='Nav'>
-        <img src={logo} alt='logo' className='logoStyle' />
+      <div className="Nav">
+        <img src={logo} alt="logo" className="logoStyle" />
       </div>
+      <div className="Container">
+        <div className="caltext">
+          {" "}
+          lets calculate distances from google map{" "}
+        </div>
 
-      <div className='Container'>
-        <div className='caltext'> lets calculate distances from google map </div>
-
-        <div className='split-part'>
-
-          <div className='left-part'>
-            Origin
-
-
-            <Autocomplete className="InputStyle">
-              <Input type='text' placeholder='Origin' ref={originRef} style={{ backgroundColor: "white" }} />
-            </Autocomplete>
-
-            <div> Stop</div>
-
-            {
-              Allitem
-            }
-
-            <button className='btn-style' onClick={AddStop}> <MdControlPoint className='addItem' /> <span>Add another stop </span></button>
-            <div className='container-item'>
+        <div className="split-part">
+          <div className="left-part">
+            <div className="left">
+              Origin
+              <Input
+                type="text"
+                className="InputStyle"
+                placeholder="Origin"
+                ref={originRef}
+                style={{ backgroundColor: "white" }}
+              />
+              <div> Stop</div>
+              <div className="group">
+                {Allitem.map((item, index) => {
+                  return (
+                    <Input
+                      type="text"
+                      className="InputStyle"
+                      placeholder="Stop"
+                      style={{ backgroundColor: "white" }}
+                      value={item.value}
+                      id="autocomplete"
+                      onChange={(e) => handleInput(e, index)}
+                    />
+                  );
+                })}
+              </div>
+              <button className="btn-style" onClick={AddStop}>
+                {" "}
+                <MdControlPoint className="addItem" />{" "}
+                <span>Add another stop </span>
+              </button>
               Destination
-              <div className='buttondis'>
-                <Autocomplete className="InputStyle">
-                  <Input
-                    type='text'
-                    placeholder='Destination'
-                    ref={destiantionRef}
-                    style={{ backgroundColor: "white" }}
-                  />
-                </Autocomplete>
-
-                <button type='submit' className='button-style' onClick={calculateRoute}>
-                  Calculate
-                </button>
+              <Input
+                type="text"
+                className="InputStyle"
+                placeholder="Destination"
+                style={{ backgroundColor: "white" }}
+                ref={destiantionRef}
+              />
+              <div className="Distance-duration">
+                <div className="Item-style">
+                  <span>Distance</span>
+                  <span className="Item">
+                    {distance ? <> {distance} Km</> : ""}{" "}
+                  </span>
+                </div>
+                <div className="Item-style">
+                  <span>Duration</span>
+                  <span className="Item">{duration}</span>
+                </div>
               </div>
-            </div>
-
-            <div className='Distance-duration'>
-              <div className='Item-style' >
-                <span>Distance</span>
-                <span className='Item'>{distance}</span>
-              </div>
-              <div className='Item-style' >
-                <span>Duration</span>
-                <span className='Item'>{duration}</span>
-              </div>
-
-            </div>
-            {
-              istrue &&
-              <div className='text-Item' >
-                The distance between  <span className='highlight'> {orgine} </span>  and<span className='highlight'>{end}</span>  the selected route is <span className='highlight'> {distance} </span> and duration is  <span className='highlight'> {duration} </span>.
-              </div>
-            }
-
-          </div>
-          <div className='right-part'>
-
-            <GoogleMap
-              center={center}
-              zoom={15}
-              mapContainerStyle={{ width: '80%', height: '60%' }}
-              options={{
-                zoomControl: false,
-                streetViewControl: false,
-                mapTypeControl: false,
-                fullscreenControl: false,
-              }}
-              onLoad={map => setMap(map)}
-            >
-              <Marker position={center} />
-              {directionsResponse && (
-                <DirectionsRenderer directions={directionsResponse} />
+              {istrue && (
+                <div className="text-Item">
+                  The distance between{" "}
+                  <span className="highlight"> {orgine} </span> to
+                  <span className="highlight">{end}</span> the selected route is{" "}
+                  <span className="highlight"> {distance} km </span> and
+                  duration is <span className="highlight"> {duration} </span>.
+                </div>
               )}
-            </GoogleMap>
-
+            </div>
+            <div className="right">
+              <button
+                type="submit"
+                className="button-style"
+                onClick={calculateRoute}
+              >
+                Calculate
+              </button>
+            </div>
+          </div>
+          <div className="right-part">
+            {Latlag.length > 0 && istrue ? (
+              <Map
+                setDistance={setDistance}
+                setDuration={setDuration}
+                routes={Latlag}
+              />
+            ) : (
+              <GoogleMap
+                center={center}
+                zoom={15}
+                mapContainerStyle={{ width: "100%", height: "100%" }}
+                options={{
+                  zoomControl: false,
+                  streetViewControl: false,
+                  mapTypeControl: false,
+                  fullscreenControl: false,
+                }}
+                onLoad={(map) => setMap(map)}
+              />
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/map.js
@@ -0,0 +1,101 @@
+import {
+  SkeletonText,
+} from '@chakra-ui/react'
+
+import { DirectionsRenderer, useJsApiLoader, GoogleMap } from '@react-google-maps/api';
+
+import React from 'react'
+
+import { useState, useEffect } from 'react'
+
+
+const defaultLocation = { lat: 40.756795, lng: -74.954298 };
+const REACT_APP_GOOGLE_MAPS_API_KEY = "AIzaSyAolXVBph__8LXk-JukgnxDUI4LPDQAsxQ"
+const Map = (props) => {
+  const [directions, setDirections] = useState(null);
+
+  const {setDistance , setDuration , routes} = props;
+
+  console.log(routes);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ['places'],
+  })
+
+  // eslint-disable-next-line no-undef
+  const travelMode = google.maps.TravelMode.DRIVING;
+
+  useEffect(() => {
+
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const routesCopy = routes.map((route) => {
+      return {
+        location: { lat: route.lat, lng: route.lag },
+        stopover: true
+      }
+    })
+    // eslint-disable-next-line no-undef
+    const origin = routesCopy.length === 1 ? new google.maps.LatLng(routesCopy[0].location.lat, routesCopy[0].location.lng) : routesCopy.shift().location
+    // eslint-disable-next-line no-undef
+    const destination = routesCopy.length === 1 ? new google.maps.LatLng(routesCopy[0].location.lat, routesCopy[0].location.lng) : routesCopy.pop().location
+    const waypoints = routesCopy
+
+
+    // here is the function for multiple routes:
+    directionsService.route(
+      {
+        origin,
+        destination,
+        travelMode,
+        waypoints
+      },
+      (result, status) => {
+        // eslint-disable-next-line no-undef
+        if (status === google.maps.DirectionsStatus.OK) {
+          
+          setDirections(result);
+          console.log(result.routes);
+          let sum = 0;
+          let time = 0;
+          for(let i  = 0; i < result.routes[0].legs.length; i++ ) {
+             sum += result.routes[0].legs[i].distance.value;
+             time += result.routes[0].legs[i].duration.value;
+          }
+
+          setDistance(sum/1000)
+          setDuration(time/60)
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
+  });
+
+  if (!isLoaded) {
+    return <SkeletonText />
+  }
+  return (
+    <div>
+      <GoogleMap
+        center={defaultLocation}
+        zoom={15}
+        mapContainerStyle={{ height: "55vh", width: "100%" }}
+        options={{
+          zoomControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+        }}
+      >
+        {directions !== null && (
+          <DirectionsRenderer directions={directions} />
+        )}
+      </GoogleMap>
+    </div>
+  );
+
+}
+
+export default Map
